aa5937906692ad117111039eb7bb9c794fa7969f
PabloRNC <pabloruanonavarro@gmail.com>
2023-07-13 15:28:08 +0200

    feat(Follower): Add Follower class which represents a Twitch follower

---
+++ packages/chatbot/src/structures/Follower.ts
@@ -0,0 +1,31 @@
+import { ChatBot } from "../ChatBot";
+import { FollowerUser } from "./FollowerUser";
+import { GetFollowers } from "@twitchapi/api-types";
+
+export class Follower{
+   
+    /**
+     * @description The current instance of the {@link ChatBot}.
+     */
+    public chatbot: ChatBot
+
+    /**
+     * @description The Twitch user of the follower.
+     */
+    public user: FollowerUser
+
+    /**
+     * @description The Date when the follower starts following the channel.
+     */
+    public followedAt: Date
+
+    public constructor(chatbot: ChatBot, data: GetFollowers){
+
+        this.chatbot = chatbot
+
+        this.user = new FollowerUser(this.chatbot, data.user_id, data.user_login, data.user_name)
+
+        this.followedAt = new Date(data.followed_at)
+
+    }
+}
