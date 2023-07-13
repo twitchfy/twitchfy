dda49e9f23ce108adc285449d05620b9c392b30d
PabloRNC <pabloruanonavarro@gmail.com>
2023-07-13 15:42:40 +0200

    docs(User): Create the User docs as there wasn't any

--- packages/chatbot/src/structures/User.ts
+++ packages/chatbot/src/structures/User.ts
@@ -4,18 +4,63 @@ import { WhisperBody } from "@twitchapi/helix"
 
 
 export class User{
+
+    /**
+     * @description The current instance of the {@link ChatBot}.
+     */
     public chatbot: ChatBot
+
+    /**
+     * @description The user's id
+     */
     public id: string
+
+    /**
+     * @description The user's login name.
+     */
     public login: string
+
+    /**
+     * @description The user's name that is displayed in the chat. This name is like the login name but can have UpperCase letters.
+     */
     public displayName: string
+
+    /**
+     * @description The user type. It can be null if the user is a normal Twitch user.
+     */
     public type: string | null
+
+    /**
+     * @description The broadcaster type. It can be an affiliate, a partner or a normal user.
+     */
     public broadcasterType: string
+
+    /**
+     * @description The user's profile description.
+     */
     public description: string
+
+    /**
+     * @description The url of the user's avatar.
+     */
     public avatarURL: string
+
+    /**
+     * @description The offline image url of the user.
+     */
     public offlineImageURL: string
+
     public viewCount: number
+
+    /**
+     * @description The string that represents when the user was created.
+     */
     public createdAt: string
 
+    /**
+     * @param chatbot 
+     * @param data 
+     */
     public constructor(chatbot: ChatBot, data: UserResponse){
         this.chatbot = chatbot
         this.id = data.id
@@ -30,6 +75,10 @@ export class User{
         this.createdAt = data.created_at
     }
 
+    /**
+     * Send a whisper to this user.
+     * @param {string} message The message you want to send to the user. You have to have the scope user:manage:whispers and the sender user has to have a verified phone number. 
+     */
     public async sendWhisper(message: string){
 
         const whisperBody = new WhisperBody(message)
@@ -37,6 +86,10 @@ export class User{
         await this.chatbot.helixClient.sendWhisper(this.chatbot.user.id, this.id, whisperBody)
     }
 
+    /**
+     * Get the whole {@link User} object with all the user's information.
+     * @returns {Promise<User>} The {@link User} of this user.
+     */
     public async fetch(): Promise<User>{
 
         return await this.chatbot.users.fetch(this.id)
