d3525f5fe2b00c308ff01e6624b92a3cb6bc97b1
PabloRNC <pabloruanonavarro@gmail.com>
2023-08-22 16:18:46 +0200

    feat(CreatedClip): Created CreatedClip class

---
+++ packages/chatbot/src/structures/CreatedClip.ts
@@ -0,0 +1,34 @@
+import { PostCreateClip } from "@twitchapi/api-types";
+import { ChatBot } from "../ChatBot";
+
+export class CreatedClip{
+
+    /**
+     * @description The current instance of the {@link ChatBot}
+     */
+    public chatbot: ChatBot
+
+    /**
+     * @description The unique identifier of the clip.
+     */
+    public id: string
+
+    /**
+     * @description The Twitch's url of the clip.
+     */
+    public url: string
+
+    /**
+     * 
+     * @param chatbot 
+     * @param data 
+     */
+    public constructor(chatbot: ChatBot, data: PostCreateClip){
+
+
+        this.chatbot = chatbot
+        this.id = data.id
+        this.url = data.edit_url.slice(0, -4)
+
+    }
+}
