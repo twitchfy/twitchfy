67eb9e90894e60bef08c986b3d50099d70c1da40
PabloRNC <pabloruanonavarro@gmail.com>
2023-08-22 16:43:50 +0200

    feat(): Add types for GetStream endpoint

---
+++ packages/api-types/src/interfaces/Stream.ts
@@ -0,0 +1,23 @@
+import { Pagination } from "./Pagination"
+
+export interface GetStreamResponse {
+    data: GetStream[]
+    pagination: Pagination
+}
+
+export interface GetStream {
+    id: string,
+    user_id: string,
+    user_login: string,
+    user_name: string,
+    game_id: string,
+    game_name: string,
+    type: string,
+    title: string,
+    tags: string[],
+    viewer_count: number,
+    started_at: string,
+    language: string,
+    thumbnail_url: string
+    is_mature: false
+}
