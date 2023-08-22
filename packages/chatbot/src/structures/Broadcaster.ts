ee39452d1a33ba1968987791ef43fde53951bd31
PabloRNC <pabloruanonavarro@gmail.com>
2023-08-22 17:47:59 +0200

    feat(stream): Add stream property to channel structures

---
+++ packages/chatbot/src/structures/Broadcaster.ts
@@ -0,0 +1,65 @@
+import { ChatBot } from "../ChatBot";
+import { WhisperBody } from "@twitchapi/helix";
+import { User } from "./User";
+
+/**
+ * @class
+ * Represents a Broadcaster
+ */
+export class Broadcaster{
+
+    /**
+     * @description The current instance of the {@link ChatBot}.
+     */
+    public chatbot: ChatBot
+
+    /**
+     * @description The login name of the broadcaster.
+     */
+    public login: string
+
+    /**
+     * @description The broadcaster's name that is displayed in the chat. This name is like the login name but can have UpperCase letters.
+     */
+
+    public displayName: string
+
+    /**
+     * @description The broadcaster's id.
+     */
+    public id: string
+
+    /**
+     * 
+     * @param chatbot 
+     * @param data 
+     */
+    public constructor(chatbot: ChatBot, id: string, login: string, displayName: string){
+        this.chatbot = chatbot
+        this.login = login
+        this.displayName = displayName
+        this.id = id
+        
+    }
+
+    /**
+     * Send a whisper to this user.
+     * @param {string} message The message you want to send to the user. You have to have the scope user:manage:whispers and the sender user has to have a verified phone number. 
+     */
+    public async sendWhisper(message: string){
+
+        const whisperBody = new WhisperBody(message)
+
+        await this.chatbot.helixClient.sendWhisper(this.chatbot.user.id, this.id, whisperBody)
+    }
+
+    /**
+     * Get the whole {@link User} object with all the user's information.
+     * @returns {Promise<User>} The {@link User} of this user.
+     */
+    public async fetch() : Promise<User> {
+        return await this.chatbot.users.fetch(this.id)
+    }
+
+
+}
---
+++ packages/chatbot/src/structures/managers/StreamManager.ts
@@ -0,0 +1,47 @@
+import { Channel } from "../Channel";
+import { ChatBot } from "../../ChatBot";
+import { PrivMSGChannel } from "../PrivMSG/PrivMSGChannel";
+import { ClearChatChannel } from "../ClearChat/ClearChatChannel";
+import { Stream } from "../Stream";
+
+/**
+ * @class
+ * Represents the StreamManager of a channel.
+ */
+export class StreamManager{
+
+   /**
+    * @description The current instance of the {@link ChatBot}.
+    */
+   public chatbot: ChatBot
+
+   /**
+    * @description The StreamManager {@link Channel}.
+    */
+   public channel: Channel | PrivMSGChannel | ClearChatChannel
+
+   /**
+    * 
+    * @param chatbot 
+    * @param channel 
+    */
+   public constructor(chatbot: ChatBot, channel: Channel | PrivMSGChannel | ClearChatChannel){
+    this.chatbot = chatbot
+    this.channel = channel
+    
+   }
+
+   /**
+    * Obtain the current stream of the channel.
+    * @returns {Promise<Stream>} Returns the channel stream if any.
+    */
+   
+   public async fetch(){
+
+    const data = await this.chatbot.helixClient.getStream(this.channel.id)
+
+    if(!data) return null
+
+    return new Stream(this.chatbot, data)
+   }
+}
