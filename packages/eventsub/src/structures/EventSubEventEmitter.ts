69f053c6cd28907c6225cd299d7b540657fcdff9
PabloRNC <pabloruanonavarro@gmail.com>
2024-01-10 18:41:47 +0100

    fix(): Fix type annotations

--- packages/eventsub/src/structures/EventSubEventEmitter.ts
+++ packages/eventsub/src/structures/EventSubEventEmitter.ts
@@ -6,15 +6,15 @@ import { EventSubEvents } from '../interfaces/EventSubEvents';
 export class EventSubEventEmitter extends EventEmitter{
 
   override on: (<K extends keyof EventSubEvents>(event: K, listener: (...args: EventSubEvents[K]) => void) => this) &
-            (<S extends string | symbol>(event: Exclude<S, keyof EventSubEvents>, listener: (...args: any[]) => void) => this);
+            (<S extends string | symbol>(event: Exclude<S, keyof EventSubEvents>, listener: (...args: any[]) => Promise<void> | void) => this);
   override emit: (<K extends keyof EventSubEvents>(event: K, ...args: EventSubEvents[K]) => boolean) &
             (<S extends string | symbol>(event: Exclude<S, keyof EventSubEvents>, ...args: any[]) => boolean);
 
   override off: (<K extends keyof EventSubEvents>(event: K, listener: (...args: EventSubEvents[K]) => void) => this) &
-            (<S extends string | symbol>(event: Exclude<S, keyof EventSubEvents>, listener: (...args: any[]) => void) => this);
+            (<S extends string | symbol>(event: Exclude<S, keyof EventSubEvents>, listener: (...args: any[]) => Promise<void> | void) => this);
 
   override once: (<K extends keyof EventSubEvents>(event: K, listener: (...args: EventSubEvents[K]) => void) => this) &
-            (<S extends string | symbol>(event: Exclude<S, keyof EventSubEvents>, listener: (...args: any[]) => void) => this);
+            (<S extends string | symbol>(event: Exclude<S, keyof EventSubEvents>, listener: (...args: any[]) => Promise<void> | void) => this);
 
   override removeAllListeners: (<K extends keyof EventSubEvents>(event?: K) => this) &
             (<S extends string | symbol>(event?: Exclude<S, keyof EventSubEvents>) => this);
