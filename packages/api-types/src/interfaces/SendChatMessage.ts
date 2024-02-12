7ff836ed24f1b20ecd495a9592be1bf2b507c5d0
PabloRNC <pabloruanonavarro@gmail.com>
2024-02-12 16:43:23 +0100

    feat(): Add types for send chat message body

--- packages/api-types/src/interfaces/SendChatMessage.ts
+++ packages/api-types/src/interfaces/SendChatMessage.ts
@@ -1,14 +1,23 @@
-export interface SendChatMessageResponse {
-    data: SendChatMessage[]
+
+export interface PostSendChatMessageBody {
+  broadcaster_id: string;
+  sender_id: string;
+  message: string;
+  reply_parent_message_id: string;
+  
 }
 
-export interface SendChatMessage {
+export interface PostSendChatMessageResponse {
+    data: PostSendChatMessage[]
+}
+
+export interface PostSendChatMessage {
     message_id: string
     is_sent: boolean
-    drop_reason: SendChatMessageDropReason | null
+    drop_reason: PostSendChatMessageDropReason | null
 }
 
-export interface SendChatMessageDropReason {
+export interface PostSendChatMessageDropReason {
     code: string
     message: string
 }
