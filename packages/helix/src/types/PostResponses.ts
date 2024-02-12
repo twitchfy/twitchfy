a8ff0015e1a312f1ccc92b0aa54f69c71df160d1
PabloRNC <pabloruanonavarro@gmail.com>
2024-02-12 16:59:42 +0100

    fix(PostResponses): Fix type PostResponses

--- packages/helix/src/types/PostResponses.ts
+++ packages/helix/src/types/PostResponses.ts
@@ -1,3 +1,3 @@
-import type { BanUserResponse, PostCreateClipResponse, PostEventSubscriptionsResponse, SendChatMessageResponse } from '@twitchapi/api-types';
+import type { BanUserResponse, PostCreateClipResponse, PostEventSubscriptionsResponse, PostSendChatMessageResponse } from '@twitchapi/api-types';
 
-export type PostResponses = Promise<BanUserResponse | PostCreateClipResponse |  PostEventSubscriptionsResponse | SendChatMessageResponse | void>
+export type PostResponses = Promise<BanUserResponse | PostCreateClipResponse |  PostEventSubscriptionsResponse | PostSendChatMessageResponse | void>
