389222c823fc7b94c394c3d2b34d8ec9a7cffba0
PabloRNC <pabloruanonavarro@gmail.com>
2024-02-12 16:22:25 +0100

    feat(): Added types for PostEventSubSubscription body

--- packages/api-types/src/interfaces/EventSub.ts
+++ packages/api-types/src/interfaces/EventSub.ts
@@ -1,25 +1,42 @@
 import type { Pagination } from './Pagination';
 
-export interface PostEventSubscriptions {
+export interface PostEventSubSubscriptionBody {
+    type: string
+    version: string
+    condition: object
+    transport: PostEventSubSubscriptionTransport
+    
+}
+
+export interface PostEventSubSubscriptionTransport {
+    method: 'webhook' | 'websocket'
+    callback?: string
+    secret?: string
+    session_id?: string
+}
+
+export interface PostEventSubSubscription {
     id: string
     status: string
     type: string
     version: string
     cost: number
     condition: object
-    transport: object
+    transport: PostEventSubSubscriptionTransport
     created_at: string
 }
 
+export interface GetEventSubSubscription extends PostEventSubSubscription {}
+
 export interface PostEventSubscriptionsResponse {
-    data: PostEventSubscriptions[]
+    data: PostEventSubSubscription[]
     total: number
     total_cost: number
     max_total_cost: number
 }
 
 export interface GetEventSubscriptions {
-    data: PostEventSubscriptions[]
+    data: GetEventSubSubscription[]
     total: number
     pagination: Pagination
     total_cost: number
