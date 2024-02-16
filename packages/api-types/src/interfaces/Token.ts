4248eeb53939d682c51e3319dc455196ab3d3b7f
PabloRNC <pabloruanonavarro@gmail.com>
2024-02-16 18:52:29 +0100

    feat(): Add ClientCredentialsResponse

--- packages/api-types/src/interfaces/Token.ts
+++ packages/api-types/src/interfaces/Token.ts
@@ -6,6 +6,12 @@ export interface TokenCodeFlowResponse {
     token_type: string
 }
 
+export interface TokenClientCredentialsFlowResponse {
+    access_token: string
+    expires_in: number
+    token_type: string
+}
+
 export interface RefreshTokenResponse {
     access_token: string
     refresh_token: string
